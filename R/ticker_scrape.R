a226551ad4fec8abc0f16ad188f6fb573f2f3545
Pedro Pereira <pedropereira@Pedros-MBP.attlocal.net>
2021-01-25 16:45:12 -0800

    Adding scraper

---
+++ R/ticker_scrape.R
@@ -0,0 +1,11 @@
+# Scraping SP500 tickers
+sp_500 <- read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies") %>%
+  html_node("table.wikitable") %>%
+  html_table() %>%
+  select(`Symbol`, Security, `GICS Sector`, `GICS Sub-Industry`) %>%
+  as_tibble()
+# Formatting names
+names(sp_500) <- sp_500 %>%
+  names() %>%
+  str_to_lower() %>%
+  make.names()
