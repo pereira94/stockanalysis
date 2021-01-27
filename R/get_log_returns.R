9db54112ab187ea7087d40fb5259fd1016e9e630
Pedro Pereira <pedropereira@Pedros-MBP.attlocal.net>
2021-01-26 19:58:16 -0800

    Adding functions

---
+++ R/get_log_returns.R
@@ -0,0 +1,20 @@
+get_log_returns <- function(x, return_format = "tibble", period = 'daily', ...) {
+  # Convert tibble to xts
+  if (!is.xts(x)) {
+    x <- xts(x[,-1], order.by = x$Date)
+  }
+  # Get log returns
+  log_returns_xts <- periodReturn(x = x$Adjusted, type = 'log', period = period, ...)
+  # Rename
+  names(log_returns_xts) <- "Log.Returns"
+  # Return in xts format if tibble is not specified
+  if (return_format == "tibble") {
+    log_returns <- log_returns_xts %>%
+      fortify.zoo() %>%
+      as_tibble() %>%
+      rename(Date = Index)
+  } else {
+    log_returns <- log_returns_xts
+  }
+  log_returns
+}
--- main.R
+++ main.R
@@ -3,3 +3,4 @@ library(stockanalysis)
 boto3 <- import("boto3")
 
 s3_client(boto3 = boto3)
+
